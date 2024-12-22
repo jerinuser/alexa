104d8652eef545ff44944dd866e32e2048cc2fb8
jerinuser <jerinarockiadass2907@gmail.com>
2024-12-22 13:28:21 +0530

    image pencil sketch code done

--- app.py
+++ app.py
@@ -1,79 +1,21 @@
-from flask import Flask, render_template, request
-import speech_recognition as sr
-import pyttsx3
-import pywhatkit
-import datetime
-import wikipedia
-import pyjokes
+import matplotlib.pyplot as plt
+import cv2
 
-app = Flask(__name__)
+image = cv2.imread("james.png")
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+inverted = 255 - gray_image
+blur = cv2.GaussianBlur(inverted, (15, 15), 0)
+invertedblur = 255 - blur
+sketch = cv2.divide(gray_image, invertedblur, scale=220.0)
 
-# Initialize the text-to-speech engine
-listener = sr.Recognizer()
-engine = pyttsx3.init()
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[1].id)
+adaptive_sketch = cv2.adaptiveThreshold(
+    sketch, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 2
+)
 
-def talk(text):
-    """Speak out the given text."""
-    engine.say(text)
-    engine.runAndWait()
 
-def take_command():
-    """Take voice command input from the microphone."""
-    command = ""
-    try:
-        with sr.Microphone() as source:
-            print('Listening...')
-            voice = listener.listen(source)
-            command = listener.recognize_google(voice)
-            command = command.lower()
-            if 'alexa' in command:
-                command = command.replace('alexa', '')
-                print(command)
-    except:
-        pass
-    return command
+cv2.imwrite("sketch_image.png", sketch)
 
-def run_alexa():
-    """Run Alexa-like voice assistant."""
-    command = take_command()
-    print(f"Command received: {command}")
-    response = ""
-
-    if 'play' in command:
-        song = command.replace('play', '')
-        response = f'Playing {song}'
-        pywhatkit.playonyt(song)
-    elif 'time' in command:
-        time = datetime.datetime.now().strftime('%I:%M %p')
-        response = f'Current time is {time}'
-    elif 'who the heck is' in command:
-        person = command.replace('who the heck is', '')
-        info = wikipedia.summary(person, 1)
-        response = info
-    elif 'date' in command:
-        response = 'Sorry, I have a headache'
-    elif 'are you single' in command:
-        response = 'I am in a relationship with WiFi'
-    elif 'joke' in command:
-        response = pyjokes.get_joke()
-    else:
-        response = 'Please say the command again.'
-
-    # Speak the response
-    talk(response)
-    return response
-
-@app.route("/", methods=["GET", "POST"])
-def index():
-    """Flask route for the main page."""
-    output = ""
-    if request.method == "POST":
-        if 'command' in request.form:
-            # Execute the run_alexa function when user submits
-            output = run_alexa()
-    return render_template("index.html", output=output)
-
-if __name__ == "__main__":
-    app.run(debug=True)
+# Display using matplotlib
+plt.imshow(sketch, cmap="gray")
+plt.axis("off")
+plt.show()
